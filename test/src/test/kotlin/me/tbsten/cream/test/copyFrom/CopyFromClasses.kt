124761bd86add02fd36be9ffffdb29d0e623c122
TBSten <81161390+TBSten@users.noreply.github.com>
2025-07-06 15:53:54 +0900

    test: add test model kdoc

--- test/src/test/kotlin/me/tbsten/cream/test/copyFrom/CopyFromClasses.kt
+++ test/src/test/kotlin/me/tbsten/cream/test/copyFrom/CopyFromClasses.kt
@@ -2,12 +2,24 @@ package me.tbsten.cream.test.copyFrom
 
 import me.tbsten.cream.CopyFrom
 
+/**
+ * Data layer model.
+ *
+ * @property prop1
+ * @property prop2
+ */
 @CopyFrom(DomainLayerModel::class)
 data class DataLayerModel(
     val prop1: String,
     val prop2: Int,
 )
 
+/**
+ * Domain layer model.
+ *
+ * @property prop1
+ * @property prop2
+ */
 @CopyFrom(DataLayerModel::class)
 data class DomainLayerModel(
     val prop1: String,
