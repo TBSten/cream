95b4703af6b06aea2e810d60df42dfa4aa63e3bf
TBSten <programmingCafeteria@gmail.com>
2025-07-07 18:17:15 +0900

    refactor: refactor unused variable name

--- cream-ksp/src/main/kotlin/me/tbsten/cream/ksp/options/CopyFunNamingStrategy.kt
+++ cream-ksp/src/main/kotlin/me/tbsten/cream/ksp/options/CopyFunNamingStrategy.kt
@@ -4,19 +4,32 @@ import com.google.devtools.ksp.symbol.KSClassDeclaration
 import me.tbsten.cream.ksp.util.fullName
 import me.tbsten.cream.ksp.util.name
 
-@Suppress("EnumEntryName")
+@Suppress("EnumEntryName", "RemoveRedundantBackticks")
 internal enum class CopyFunNamingStrategy(val funName: (source: KSClassDeclaration, target: KSClassDeclaration) -> String) {
     `under-package`({ _, target ->
         target.fullName.replace(target.packageName.asString() + ".", "")
     }),
-    `diff-parent`(
-        { source, target -> target.fullName.replace(source.fullName, "") }
+    `diff`(
+        { source, target ->
+            val sourceName = source.fullName
+            val targetName = target.fullName
+            var i = 0
+            val minLength = minOf(sourceName.length, targetName.length)
+
+            // 共通部分をスキップ
+            while (i < minLength && sourceName[i] == targetName[i]) {
+                i++
+            }
+
+            // second だけにある差分
+            targetName.substring(i)
+        }
     ),
     `simple-name`(
         { _, target -> target.simpleName.asString() }
     ),
     `full-name`(
-        { source, target -> target.fullName }
+        { _, target -> target.fullName }
     ),
     `inner-name`(
         { _, target ->
