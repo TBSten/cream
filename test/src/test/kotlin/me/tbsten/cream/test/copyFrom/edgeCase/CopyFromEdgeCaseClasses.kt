5965e3070801ca7ea728b49de1d97ffc651b2176
TBSten <programmingCafeteria@gmail.com>
2025-08-06 01:20:59 +0900

    feat: Add test cases

--- test/src/test/kotlin/me/tbsten/cream/test/copyFrom/edgeCase/CopyFromEdgeCaseClasses.kt
+++ test/src/test/kotlin/me/tbsten/cream/test/copyFrom/edgeCase/CopyFromEdgeCaseClasses.kt
@@ -70,14 +70,19 @@ data class VisibilitySource(
 )
 
 // 6. Property mapping with @CopyFrom.Map
-@CopyFrom(DataModel::class)
+@CopyFrom(DataModel::class, UiModel::class)
 data class DomainModel(
-    @CopyFrom.Map("dataId")
+    @CopyFrom.Map("dataId", "uiId")
     val domainId: String,
     val name: String,
 )
 
 data class DataModel(
     val dataId: String,
+    val name: String,
+)
+
+data class UiModel(
+    val uiId: String,
     val name: String,
 )
